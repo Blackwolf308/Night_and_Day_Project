d232271cf2ff97258baa44f6b3a6b3b409a264c9
Blackwolf308 <blackwolf3012@live.com>
2016-03-06 17:30:42 -0500

    edited moon

--- nightday.js
+++ nightday.js
@@ -2,17 +2,20 @@ $(document).ready(function () {
     $('#orb').click(function () {
         if ($('#orb').hasClass('sun')) {
             $('#orb').removeClass('sun').addClass('moon');
-        }
-        else {
+        } else {
             $('#orb').removeClass('moon').addClass('sun');
         }
         if ($('#sky').hasClass('day')) {
             $('#sky').removeClass('day').addClass('night');
             console.log('its Night');
-        }
-        else {
+        } else {
             $('#sky').removeClass('night').addClass('day');
             console.log('its Day');
+        }
+        if ($('#moonspot1').hasClass('visible')) {
+            $('#moonspot1').removeClass('visible');
+        } else {
+            $('#moonspot1').addClass('visible');
         }
     });
 });
